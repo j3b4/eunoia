6d961f771812a9bcde9f25a262aa2c260582b2ed
j3b4 <juggins@gmail.com>
2021-01-30 13:35:30 -0400

    silence updated

--- commands/silence.py
+++ commands/silence.py
@@ -1,5 +1,5 @@
 # The Silence Module overloads built in Evennia communication commands
-# and ups their permissions so that only admins can use them. 
+# and ups their permissions so that only staff can use them. 
 
 from evennia import default_cmds
 from evennia import CmdSet, Command
@@ -23,3 +23,31 @@ class CmdSetDesc(default_cmds.CmdSetDesc):
     key = 'setdesc'
     locks = 'cmd:perm(Builder)'
 
+#
+# Hmm
+#
+
+#        self.remove(default_cmds.CmdAddCom)
+#        self.remove(default_cmds.CmdAllCom)
+#        self.remove(default_cmds.CmdCBoot)
+#        self.remove(default_cmds.CmdDelCom)
+#        self.remove(default_cmds.CmdChannels)
+#        self.remove(default_cmds.CmdCdestroy)
+class CmdChannelCreate(default_cmds.CmdChannelCreate):
+    locks = 'cmd:perm(Helper)'
+
+
+class CmdChannelCdestroy(default_cmds.CmdCDestroy):
+    locks = 'cmd:perm(Helper)'
+
+
+class CmdChannels(default_cmds.CmdChannels):
+    locks = 'cmd:perm(Helper)'
+        
+#        self.remove(default_cmds.CmdClock)
+#        self.remove(default_cmds.CmdCBoot)
+#        self.remove(default_cmds.CmdCemit)
+#        self.remove(default_cmds.CmdCWho)
+#        self.remove(default_cmds.CmdCdesc)
+#        self.remove(default_cmds.CmdChannels)
+#        self.remove(default_cmds.CmdPage)
