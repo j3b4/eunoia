e1dc996df73b94acf90d4d8c294aa1391f63a8f0
j3zibl <j3b@3b1.org>
2021-02-04 23:14:47 -0400

    oob added to body_cmdset

--- commands/body_cmdsets.py
+++ commands/body_cmdsets.py
@@ -1,7 +1,7 @@
 # commands/body_cmdsets.py
 
 from evennia import CmdSet
-from commands.body import CmdPing
+from commands.body import CmdPing CmdOOB
 from evennia.commands.default import account, help
 
 
@@ -12,6 +12,7 @@ class CmdSetBody(CmdSet):
 
     def at_cmdset_creation(self):
         self.add(CmdPing())
+        self.add(CmdOOB())
         self.add(account.CmdWho())
         self.add(account.CmdQuit())
         self.add(account.CmdOOC())
